4f572c6fd0bf527ed2659dced06ad1eaa7789fb5
Luis Daniel Caraballo <luisred61@gmail.com>
2019-06-12 15:25:51 -0500

    Se cambian datos estaticos por datos dinamicos

--- src/Invie.js
+++ src/Invie.js
@@ -4,12 +4,62 @@ import './css/invie.css';
 import Portada from "./components/Portada.jsx";
 import Guitarras from "./components/Guitarras.jsx";
 import Footer from "./components/Footer.jsx";
+import logoPortada from "./images/invie.png";
+import acustica from "./images/invie-acustica.png"
+import clasica from "./images/invie-classic.png"
+
+const data = {
+  menu: [
+    {
+        href: "index.html",
+        title: "Home",
+    },
+    {
+        href: "#guitarras",
+        title: "Guitarras",
+    },
+    {
+        href: "precio.html",
+        title: "Precios",
+    },
+    /* {
+        href: "hola.html",
+        title: "Don't click me",
+    } */
+  ],
+  logoPortada : logoPortada,
+  
+  guitarras: [
+    {
+        image: acustica,
+        alt: "Guitarra Invie Acustica",
+        name: "Invie Acustica",
+        features: [
+            "Estilo vingate",
+            "Madera Pura",
+            "Incluye estuche invisible de aluminio"
+        ]
+    },
+    {
+      image: clasica,
+      alt: "Guitarra Invie Clasica",
+      name: "Invie Clasica",
+      features: [
+          "Estilo vingate",
+          "Liviana",
+          "Inicia tu camino como Rockstar"
+      ]
+  }
+]
+}
+
+
 
 function App() {
   return (
     <div className="Invie">
-      <Portada/>
-      <Guitarras/>
+      <Portada menu={data.menu} logo={data.logoPortada}/>
+      <Guitarras guitarras={data.guitarras}/>
       <Footer/>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import Invie from './Invie';
 import * as serviceWorker from './serviceWorker';
 
+
 ReactDOM.render(<Invie />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
